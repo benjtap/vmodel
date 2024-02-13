d2bc1b1531ee4a39bb9a6a9941174ab147047f96
benjtap <bntapiero@gmail.com>
2024-02-13 12:28:15 +0200

    one

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import Controller from './components/Controller.vue';
+  import { reactive } from 'vue'; 
+  const object1 = reactive({name: "Bruce", age: 38});
+  const object2 = reactive({name: "Alex", age: 6});
+</script>
+
+<template>
+  <div>
+    {{object1}}<br/>
+    {{object2}}
+    <Controller :my-object="object1"/>
+    <Controller :my-object="object2"/>
+  </div>
+</template>
---
+++ src/components/Controller.vue
@@ -0,0 +1,35 @@
+<script setup>
+  import { computed } from 'vue'
+
+  const props = defineProps({
+    myObject: {
+      type: Object,
+      default: () => {}
+    }
+  })
+
+  const name = computed({
+    get () {
+      return props.myObject.name
+    },
+    set (value) {
+     props.myObject.name = value
+    }
+  })
+
+  const age = computed({
+    get () {
+      return props.myObject.age
+    },
+    set (value) {
+     props.myObject.age = parseInt(value)
+    }
+  })
+</script>
+
+<template>
+  <div>
+    <input v-model="name"/><br/>
+    <input v-model="age" type="number"/>
+  </div>
+</template>
